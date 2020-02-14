25051c90314465e4778f6c69947d55f37db7941f
Paul Debevec <paulmdebevec@gmail.com>
2020-02-14 10:42:17 -0700

    Add functionality for movie show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,11 @@
 class Movie < ApplicationRecord
   validates_presence_of :title
   belongs_to :studio
-  
+
   has_many :actor_movies
   has_many :actors, through: :actor_movies
+
+  def actors_by_age
+    actors.order(age: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
-
   get '/actors/:actor_id', to: 'actors#show'
+  get '/movies/:movie_id', to: 'movies#show'
 end
