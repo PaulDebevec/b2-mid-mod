c32ce8bd6b64fc7249247b98ed63b4aee7474979
Paul Debevec <paulmdebevec@gmail.com>
2020-02-14 11:54:25 -0700

    Final commit

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,14 @@
 class Actor < ApplicationRecord
   validates_presence_of :name, :age
-  
+
   has_many :actor_movies
   has_many :movies, through: :actor_movies
+
+  def associates
+    # I still need work on joins. Not really sure how to do this one.
+
+    # require "pry"; binding.pry
+    # Movie.select(:title).joins(:actor_movies).distinct
+    # Actor.select(:name).joins(:actor_movies).where("actor_id = movie.id")
+  end
 end
--- spec/features/actors/show_spec.rb
+++ spec/features/actors/show_spec.rb
@@ -50,5 +50,42 @@ describe "Actors Show Page" do
         expect(page).to have_content(movie_4.title)
       end
     end
+
+    it "I see a unique list of all the actors this particular actor has worked with" do
+      studio_2 = Studio.create(name: "Disney")
+
+      actor_1 = Actor.create!(name: "Daniel Mackey", age: 23)
+      actor_2 = Actor.create!(name: "Chris Fisher", age:42)
+      actor_3 = Actor.create!(name: "John Smith", age: 32)
+      actor_4 = Actor.create!(name: "Anduin Wrynn", age: 28)
+
+      movie_1 = studio_2.movies.create!(title: "A New Hope", creation_year: 1978, genre: "Debatably-Space-Opera")
+      movie_2 = studio_2.movies.create!(title: "The Empire Strikes Back", creation_year: 1982, genre: "Debatably-Space-Opera")
+
+      actor_1.movies << movie_1
+      actor_3.movies << movie_1
+      actor_4.movies << movie_1
+
+      actor_2.movies << movie_2
+      actor_3.movies << movie_2
+
+      visit "/actors/#{actor_1.id}"
+
+      within "#worked-with" do
+        expect(page).to have_content("#{actor_3.name}")
+        expect(page).to have_content("#{actor_4.name}")
+      end
+
+      visit "/actors/#{actor_3.id}"
+
+      within "#worked-with" do
+        expect(page).to have_content("#{actor_1.name}")
+        expect(page).to have_content("#{actor_2.name}")
+        expect(page).to have_content("#{actor_4.name}")
+      end
+    end
   end
 end
+
+# When I visit an actor's show page
+# I see a unique list of all of the actors this particular actor has worked with.
