f8facd0eeebbf7cabef78fd2498cf3851e4098f0
Paul Debevec <paulmdebevec@gmail.com>
2020-02-14 10:42:01 -0700

    Add model test for actors_by_age to sort actors by age in desc order

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Movie, type: :model do
     it {should have_many(:actor_movies)}
     it {should have_many(:actors).through(:actor_movies)}
   end
+
+  describe "model methods" do
+    it "actors_by_age" do
+      studio_1 = Studio.create(name: "Universal Studio")
+
+      actor_2 = Actor.create!(name: "Chris Fisher", age:22)
+      actor_3 = Actor.create!(name: "John Smith", age: 32)
+      actor_4 = Actor.create!(name: "Anduin Wrynn", age: 28)
+
+      movie_4 = studio_1.movies.create!(title: "Jurassic Park", creation_year: 1991, genre: "Thriller")
+
+      actor_2.movies << movie_4
+      actor_3.movies << movie_4
+      actor_4.movies << movie_4
+
+      expect(movie_4.actors_by_age).to eq([actor_3, actor_4, actor_2])
+    end
+  end
 end
