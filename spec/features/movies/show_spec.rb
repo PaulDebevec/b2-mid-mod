2acd2e88672c74fc26556ecbdeb447269777f9cc
Paul Debevec <paulmdebevec@gmail.com>
2020-02-14 10:31:14 -0700

    Add it block in test

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -9,45 +9,47 @@ require 'rails_helper'
 
 describe "Movie Show Page" do
   describe "As a visitor" do
-    studio_1 = Studio.create(name: "Universal Studio")
-    studio_2 = Studio.create(name: "Disney")
+    it "shows the movie title, creation year, and genre as well as the actor names in descending order by age" do  
+      studio_1 = Studio.create(name: "Universal Studio")
+      studio_2 = Studio.create(name: "Disney")
 
-    actor_1 = Actor.create!(name: "Daniel Mackey", age: 23)
-    actor_2 = Actor.create!(name: "Chris Fisher", age:42)
-    actor_3 = Actor.create!(name: "John Smith", age: 32)
-    actor_4 = Actor.create!(name: "Anduin Wrynn", age: 28)
+      actor_1 = Actor.create!(name: "Daniel Mackey", age: 23)
+      actor_2 = Actor.create!(name: "Chris Fisher", age:42)
+      actor_3 = Actor.create!(name: "John Smith", age: 32)
+      actor_4 = Actor.create!(name: "Anduin Wrynn", age: 28)
 
-    movie_1 = studio_2.movies.create!(title: "A New Hope", creation_year: 1978, genre: "Debatably-Space-Opera")
-    movie_2 = studio_2.movies.create!(title: "The Empire Strikes Back", creation_year: 1982, genre: "Debatably-Space-Opera")
-    movie_3 = studio_2.movies.create!(title: "Spiderman", creation_year: 2014, genre: "Action")
-    movie_4 = studio_1.movies.create!(title: "Jurassic Park", creation_year: 1991, genre: "Thriller")
-    movie_5 = studio_1.movies.create!(title: "Jaws", creation_year: 1993, genre: "Thriller")
+      movie_1 = studio_2.movies.create!(title: "A New Hope", creation_year: 1978, genre: "Debatably-Space-Opera")
+      movie_2 = studio_2.movies.create!(title: "The Empire Strikes Back", creation_year: 1982, genre: "Debatably-Space-Opera")
+      movie_3 = studio_2.movies.create!(title: "Spiderman", creation_year: 2014, genre: "Action")
+      movie_4 = studio_1.movies.create!(title: "Jurassic Park", creation_year: 1991, genre: "Thriller")
+      movie_5 = studio_1.movies.create!(title: "Jaws", creation_year: 1993, genre: "Thriller")
 
-    actor_1.movies << movie_1
-    actor_1.movies << movie_2
-    actor_2.movies << movie_1
-    actor_2.movies << movie_2
-    actor_3.movies << movie_3
-    actor_3.movies << movie_4
-    actor_4.movies << movie_4
-    actor_4.movies << movie_5
+      actor_1.movies << movie_1
+      actor_1.movies << movie_2
+      actor_2.movies << movie_1
+      actor_2.movies << movie_2
+      actor_3.movies << movie_3
+      actor_3.movies << movie_4
+      actor_4.movies << movie_4
+      actor_4.movies << movie_5
 
-    visit "/movies/#{movie_1.id}"
+      visit "/movies/#{movie_1.id}"
 
-    expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_1.title)
 
-    within "#actors" do
-      expect(page).to have_content(actor_1.name)
-      expect(page).to have_content(actor_2.name)
-    end
+      within "#actors" do
+        expect(page).to have_content(actor_1.name)
+        expect(page).to have_content(actor_2.name)
+      end
 
-    visit "/movies/#{movie_4.id}"
+      visit "/movies/#{movie_4.id}"
 
-    expect(page).to have_content(movie_4.title)
+      expect(page).to have_content(movie_4.title)
 
-    within "#actors" do
-      expect(page).to have_content(actor_3.name)
-      expect(page).to have_content(actor_4.name)
+      within "#actors" do
+        expect(page).to have_content(actor_3.name)
+        expect(page).to have_content(actor_4.name)
+      end
     end
   end
 end
